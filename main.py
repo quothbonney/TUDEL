44e151b9c411e534e750a112012c8446a4a0dc89
Jackie Carson <100622290+quothbonney@users.noreply.github.com>
2022-06-09 21:58:24 -0500

    Minimum Viable Product - threshold function

--- main.py
+++ main.py
@@ -63,14 +63,34 @@ def deposition_mask(res, type_string):
     mask = cv2.inRange(image, lower, upper)
 
     deposit = cv2.bitwise_and(res, res, mask=mask)
-    img_gray = cv2.cvtColor(deposit, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(image=img_gray, threshold1=30, threshold2=50)
-    cv2.imshow("Deposit", deposit)
-    cv2.imshow(type_string + " Mask", edges)
+    kernel = np.ones((2, 2))
+    erosion = cv2.erode(deposit, kernel, iterations=1)
 
+    cv2.imshow("Deposit", erosion)
+    return erosion
+
+
+def threshold(res, image):
+    ret, thresh = cv2.threshold(res, 128, 255, 0)
+    contours, heirarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+
+    areas = []
+    for cnt in contours:
+        M = cv2.moments(cnt)
+        area = cv2.contourArea(cnt)
+        if area < 20 and area > 8:
+            areas.append(area)
+
+    cv2.imshow("Image", image)
+    print(sum(areas))
 
 if __name__ ==  '__main__':
-    image = cv2.imread('imgs/img6 PbI2.jpg')
+    FILEPATH = 'imgs/img6 PbI2.jpg'
+    TYPE_STRING = "PbI2"
+
+    image = cv2.imread(FILEPATH)
     result = image.copy()
     b_val = red_brightness(result)
     if b_val < 110:
@@ -86,8 +106,13 @@ if __name__ ==  '__main__':
         result = increase_brightness(result, 5)
         b_val = red_brightness(result)
 
-    cv2.imshow("final", result)
-    deposition_mask(result, "PbI2")
+    #cv2.imshow("final", result)
+    deposit = deposition_mask(result, TYPE_STRING)
+    img_gray = cv2.cvtColor(deposit, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(image=img_gray, threshold1=30, threshold2=50)
+
+    threshold(edges, image)
+
 
 
     cv2.waitKey(0)
