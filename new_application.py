cc9da6fb3ace530b9e24157891570af033c77048
Jackie Carson <100622290+quothbonney@users.noreply.github.com>
2023-05-31 12:16:23 -0500

    Continuing work

--- new_application.py
+++ new_application.py
@@ -1,15 +1,33 @@
+import queue
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 from PIL import ImageTk, Image
 import cv2
 import numpy as np
+from tkinter import scrolledtext
 import webbrowser
 from tkinter import messagebox
 from src.calibrate import calibrate
 from src import dimensions, analysis, selection
 from src.mask import Mask
-import json, threading
-import os
+import json, threading, logging
+
+
+class SingletonTextHandler:
+    _instance = None
+    # Holds messages in an array, indexes the last element of the array
+    messages = [" "]
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(SingletonTextHandler, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    @classmethod
+    def add_message(cls, message):
+        cls.messages.append(message)
+
 
 class Application(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -37,6 +55,8 @@ class ImageWranger:
         except:
             return
 
+        SingletonTextHandler.add_message(f"Opened image {imageselect} successfully")
+
     def write_image(self):
         ret = cv2.cvtColor(self.right, cv2.COLOR_BGR2RGB)
 
@@ -44,6 +64,9 @@ class ImageWranger:
         if not filename:
             return
         cv2.imwrite(f"{filename}.png", ret)
+        SingletonTextHandler.add_message(f"Wrote image successfully to {filename}")
+
+        return True
 
 
 class ImageView(tk.Label):
@@ -60,8 +83,32 @@ class ImageView(tk.Label):
         self.image = ImageTk.PhotoImage(img)
         self.config(image=self.image)
 
+class ConsoleView(scrolledtext.ScrolledText):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.configure(height="12", padx="12", pady="12", wrap=tk.WORD, state="disabled")
+        self.q = SingletonTextHandler()
 
-class Interface(tk.Frame):
+    def update(self):
+        # Indexes the most recent message in the message buffer and writes it
+        message = self.q.messages[-1]
+        self.write_message(message)
+
+        print(self.q.messages[-1])
+
+    def write_message(self, message):
+        # For some reason the ScrollingText will not update unless it is marked as being in the normal state
+        # We are just rolling with it. a 2ms flicker is the least awful thing about this program
+        self.configure(state="normal")
+        self.insert(tk.END, message + '\n')
+        self.configure(state="disabled")
+
+    def clear_console(self):
+        """Clears the console view."""
+        self.delete('1.0', tk.END)
+
+
+class Interface(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
@@ -73,30 +120,49 @@ class Interface(tk.Frame):
     def create_widgets(self):
         self.master.title("TU Digital Electrochemistry Lab")
 
-        # ------------- Image Boxes -------------
+        # ------------- Image Boxes/Labels -------------
         # Labels for Left side
-        self.label_left = tk.Label(self, text="Original Image", font="bold")
+        self.label_left = ttk.Label(self, text="Original Image", font="bold")
         self.label_left.grid(row=0, column=0)
         # Bounding box of image
-        self.label_view_left = tk.Label(self, text="Modified", height="25", width="52", bd=0.5, relief="solid")
+        self.label_view_left = tk.Label(self, text="Modified", height="19", width="52", bd=0.5, relief="solid")
         self.label_view_left.grid(row=1, column=0, pady=10, padx=10)
         # ImageView containing the original image
         self.view_left = ImageView(self, image=None)
         self.view_left.grid(row=1, column=0)
 
         # Same thing going on here
-        self.label_right = tk.Label(self, text="Modified Image", font="bold")
+        self.label_right = ttk.Label(self, text="Modified Image", font="bold")
         self.label_right.grid(row=0, column=1)
-        self.label_view_right = tk.Label(self, text="Modified", height="25", width="52", bd=0.5, relief="solid")
+        self.label_view_right = tk.Label(self, text="Modified", height="19", width="52", bd=0.5, relief="solid")
         self.label_view_right.grid(row=1, column=1, pady=10, padx=10)
         self.view_right = ImageView(self, text="Modified")
         self.view_right.grid(row=1, column=1)
 
         # -------------- Buttons ---------------
-        self.open_button = tk.Button(self, text="Open Image", command=lambda: (self.image_handler.select_image(), self.update_image_interface(2)))
+        self.open_button = ttk.Button(self, text="Open Image", command=lambda: (self.image_handler.select_image(), self.update_image_interface(2), self.console.update()))
         self.open_button.grid(row=2, column=0)
 
+        self.save_button = ttk.Button(self, state="disabled", text="Save Image", command=lambda: (self.image_handler.write_image(), self.update_image_interface(2), self.console.update()))
+        self.save_button.grid(row=2, column=1)
+
+        self.divider = ttk.Separator(self, orient="vertical")
+        self.divider.grid(row=1, column=2)
+
+        self.save_button = ttk.Button(self, state="disabled", text="Save Image", command=lambda: (self.image_handler.write_image(), self.update_image_interface(2), self.console.update()))
+        self.save_button.grid(row=0, column=3)
+
+        # -------------- Console ---------------
+        self.console = ConsoleView(self)
+        self.console.grid(column=0, row=3, columnspan=2, sticky=(tk.N, tk.S, tk.W, tk.E))
+
     def update_image_interface(self, side: int):
+        # Change button states depending on image loaded
+        if self.image_handler.left is not None:
+            self.save_button.config(state="normal")
+        else:
+            self.save_button.config(state="disabled")
+
         if side not in (0, 1, 2):
             raise Exception("Side must be (0, 1, 2)")
 
